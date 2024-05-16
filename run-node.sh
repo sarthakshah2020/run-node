8988b8a58ffbb316cd7ccbff0fa603cb3dfbd522
sarthakshah2020 <167911799+sarthakshah2020@users.noreply.github.com>
2024-05-16 13:40:12 +0530

    Update run-node.sh

--- run-node.sh
+++ run-node.sh
@@ -3,3 +3,5 @@ cd ~ && git clone https://github.com/QuilibriumNetwork/ceremonyclient.git
 cd ~/ceremonyclient/client && GOEXPERIMENT=arenas go build -o qclient main.go
 
 tmux new-session -d -s quil 'export PATH=$PATH:/usr/local/go/bin && cd ~/ceremonyclient/node && ~/scripts/qnode_restart.sh'
+
+tmux a -t quil
