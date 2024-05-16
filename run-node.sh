05f2ec21cd3e19ff3d057a3ec3d3bb9a01b6fb86
sarthakshah2020 <167911799+sarthakshah2020@users.noreply.github.com>
2024-05-16 13:40:59 +0530

    Update run-node.sh

--- run-node.sh
+++ run-node.sh
@@ -5,3 +5,4 @@ cd ~/ceremonyclient/client && GOEXPERIMENT=arenas go build -o qclient main.go
 tmux new-session -d -s quil 'export PATH=$PATH:/usr/local/go/bin && cd ~/ceremonyclient/node && ~/scripts/qnode_restart.sh'
 
 tmux a -t quil
+
